adbf096eff09261731bf0cd99977b2b4413053e7
Victor Lin <linvictorw@gmail.com>
2022-04-04 16:32:28 +0800

    Completed activity

--- reflection.go
+++ reflection.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 //Declare Inspection Function
+func inspector(x interface{}) {
+	typ := reflect.TypeOf(x)
+	fmt.Println("")
+	fmt.Println(x)
+	fmt.Println(typ.Name())
+	fmt.Println(typ.Kind())
+}
 
 func main() {
 	//Insert code here
+	str := "This is a string"
+	integer := 12345
+	float := 1.2345
+	boolean := true
+
+	inspector(str)
+	inspector(integer)
+	inspector(float)
+	inspector(boolean)
 }
